ea9a0db9bf20af5413cb23653f630d90d63a3d19
ali aalipour <ali.aalipour.72@hotmail.com>
2024-06-12 22:08:55 +0330

    init

---
+++ MauiAppMudB/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppMudB
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MauiAppMudB/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppMudB
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
